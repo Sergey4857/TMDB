36fce5596fb5a81eaa307c3eca1d55b16185a0fc
Sergey4857 <yasak.sergey@gmail.com>
2023-08-29 00:25:23 +0300

    fx

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/TMDB-typescript">
       <App />
     </BrowserRouter>
   </React.StrictMode>
