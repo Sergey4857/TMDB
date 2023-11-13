401490e7c33f32c5487731a6a6bda2ac92ed5b41
Sergey4857 <yasak.sergey@gmail.com>
2023-11-13 16:37:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/TMDB-typescript">
+    <BrowserRouter basename="/TMDB">
       <App />
     </BrowserRouter>
   </React.StrictMode>
